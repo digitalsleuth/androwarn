e11f06b80b2c55d861fd40bd26c6daacc8092b2b
Corey <corey@digitalsleuth.ca>
2020-11-21 19:04:00 +0000

    Added setup file

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding='utf8') as readme:
+    long_description = readme.read()
+
+setup(
+    name="androwarn",
+    version="1.6",
+    author="maaaaaz",
+    license="GNU GPLv3",
+    url="https://github.com/digitalsleuth/androwarn",
+    description= ("Yet another static code analyzer for malicious Android applications"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "androguard >= 3.2.1",
+        "future",
+        "jinja2",
+        "play_scraper",
+    ],
+    entry_points={
+        "console_scripts": [
+            "androwarn = androwarn.androwarn:main",
+        ],
+    },
+    package_data={'': ['*.*', 'README.md', 'COPYING', 'COPYING.LESSER']},
+)
+
